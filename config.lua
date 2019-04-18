f3bbcd25c152a8d494df83d222ec79ef9c2f1e95
svengineer99 <svengineer99@yahoo.com>
2019-04-17 22:28:47 -0700

    Add files via upload

--- config.lua
+++ config.lua
@@ -1,6 +1,153 @@
-return mwse.loadConfig("graphicHerbalism") or {
-    volume = 1.0,
+ return mwse.loadConfig("graphicHerbalism") or {
+
+-- sveng edit begin
+    pluralQuantityIdStubList = {
+-- note: use only normal nouns that can be made plural by added "s"
+--        { "", nil }, -- uncomment this line to disable plural prefixing
+        { "fungus", "section" },
+        { "mushr", "section" },
+	},
+    pluralQuantityNameStubList = {
+-- note: use only normal nouns that can be made plural by added "s"
+        { "jerky$", "sample" }, -- "cut"
+        { "meat$", "sample" }, -- "cut"
+        { "sausauge", "sample" }, -- "cut"
+        { "cane$", "sample" },
+        { "lard$", "sample" },
+        { "seeds$", "sample" }, -- "handful"
+        { "powder$", "sample" },
+        { "dust$", "sample" },
+        { "oil$", "sample" },
+        { "pollen$", "sample" },
+        { "nectar$", "sample" },
+        { "lichen$", "sample" },
+        { "moss$", "sample" },
+        { "pulp$", "sample" },
+        { "muck$", "sample" },
+        { "roobrush$", "sample" },
+        { "motherw", "blossom" }, -- "blossom"
+        { "nectar sac", "sample" }, 
+        { "sap$", "sample" }, -- "dollop"
+        { "honey$", "sample" }, -- "dollop"
+        { "roses$", "bouquet" }, -- "bouquet"
+        { "flowers$", "posy" }, -- "posy"
+        { "anther$", "posy" }, -- "posy"
+        { "kanet$", "posy" }, -- "posy"
+        { "roland's tears$", "posy" }, -- "posy"
+	{ "heather$", "nosegay" }, -- "nosegay"
+        { "cap$", "section" }, 
+        { "caps$", "sample" }, -- "troop"
+        { "ginseng$", "sample" }, 
+        { "mushroom$", "section" }, -- "troop"
+        { "mushrooms$", "sample" }, -- "troop"
+        { "fungus$", "section" }, -- "cluster"
+        { "polypore", "section" },
+        { "foot$", "section" },
+        { "fiber$", "section" },
+        { "funnel$", "sample" }, -- cluster
+        { "leaves$", "sample" },
+        { "monkshood$", "sample" },
+        { "leaf$", "sampling" },
+        { "marshmerrow$", "stalk" },
+        { "reeds$", "bundle" },
+        { "grass$", "bundle" },
+        { "petals$", "sample" }, -- "bloom"
+        { "stalks$", "bundle" },
+        { "weed$", "sample" },
+        { "weeds$", "bundle" },
+        { "berry$", "sample" },
+        { "berries$", "cluster" },
+        { "grape$", "cluster" },
+        { "grapes$", "cluster" },
+        { "salts$", "measure" },
+        { "salt$", "sample" },
+        { "pie$", "slice" },
+        { "nuts$", "sample" }, -- "handful"
+        { "bloat$", "sample" },
+        { "yam$", "sample" },
+        { "root$", "sample" },
+        { "^ore", "sample" }, -- "slug"
+        { " ore$", "sample" }, -- "slug"
+        { "^raw ", "sample" }, -- "slug"
+        { "metal", "sample" }, -- "piece"
+        { "scathecraw$", "portion" },
+        { "wheat$", "portion" },
+        { "grain$", "portion" },
+        { "rye$", "portion" },
+        { "barley$", "portion" },
+        { "corn$", "portion" },
+        { "indureta$", "portion" },
+        { "rice$", "portion" },
+        { "oat$", "portion" },
+-- ends in s non-plural exceptions	
+        { "coprinus$", nil }, -- violet coprinus
+        { "hibiscus$", nil }, -- violet coprinus
+-- otherwise ends in s plural default	
+        { "s$", "sample" },
+	},
+    pickVerbNameStubList = {
+--        { "", "pick" }, -- uncomment this line to use pick for all
+        { "flower", "pick" },
+        { "hyacinth$", "pick" },
+        { "anther$", "pick" },
+        { "nectar sac", "pick" },
+        { "kanet$", "pick" },
+        { "bell$", "pick" },
+        { "berries$", "pick" },
+        { "berry$", "pick" },
+        { "motherw", "pick" },
+        { "petal$", "pick" },
+        { "rose$", "pick" },
+        { "fruit$", "pick" },
+        { "grape$", "pick" },
+        { "apple$", "pick" },
+        { "plumb$", "pick" },
+        { "fig$", "pick" },
+        { "blossom$", "pick" },
+        { "grain$", "harvest" },
+        { "indureta$", "harvest" },
+        { "rice$", "harvest" },
+        { "wheat$", "harvest" },
+        { "rye$", "harvest" },
+        { "barley$", "harvest" },
+        { "corn$", "harvest" },
+        { "oat$", "harvest" },
+        { "scathecraw$", "harvest" },
+        { "roobrush", "pick" },
+        { "pearl", "extract" },
+        { "fiber", "harvest" },
+        { "tuber", "harvest" },
+        { "bloat", "harvest" },
+        { "pulp", "extract" },
+        { "muck", "extract" },
+        { "root", "pick" },
+        { "yam", "pick" },
+        { "^raw ", "excavat" },
+        { " ore$", "excavat" },
+	},
+    pickVerbIdStubList = {
+        { "kollop", "extract" },
+        { "rock_", "excavat" },
+        { "_ore", "excavat" },
+        { "ingmine_", "excavat" },
+        { "_mine_", "excavat" },
+-- note: last entry below is default if none above match	
+        { "", "collect" }, -- "gather"
+	},
+    pickedVerb = {
+        ["pick"] = "pick",
+        ["harvest"] = "harrow",
+        ["extract"] = "extract",
+        ["excavat"] = "deplet",
+        ["collect"] = "loot",
+	},
+    showPicked = true,
+    showIngrNameIfNotSameAsCont = true,
+
+-- sveng edit end
+
     showTooltips = true,
+    volume = 100,
     blacklist = {
         -- vanilla content
         ["barrel_01_ahnassi_drink"] = true,
